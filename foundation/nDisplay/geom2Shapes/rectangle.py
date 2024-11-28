8893a0d0db856aa94a578004b0d41f73aa9ec6ab
chartan1990 <charissatanweiyi@gmail.com>
2024-11-28 17:01:37 +0800

    planning
    find resources for OpenGL, use it to draw 2D Graphs...

--- foundation/nDisplay/geom2Shapes/rectangle.py
+++ foundation/nDisplay/geom2Shapes/rectangle.py
@@ -1,5 +1,13 @@
 import OpenGL.GL as gl
 
+#2D shapes needed
+# OVAL
+# LINE
+# TEXT
+## http://jerome.jouvie.free.fr/opengl-tutorials/Tutorial18.php # 2DFontPart1
+## http://jerome.jouvie.free.fr/opengl-tutorials/Tutorial19.php # 3D Font
+## http://jerome.jouvie.free.fr/opengl-tutorials/Tutorial20.php # 2DFontPart2
+
 def rectangle(vertices):
     gl.glBegin(gl.GL_QUADS)
     for vertex in vertices:
